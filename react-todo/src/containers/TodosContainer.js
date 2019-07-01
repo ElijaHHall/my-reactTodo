a94cef0fed6c03098b3041a51fbeec254e15c473
Elijah Hall <ehall851@gmail.com>
2019-07-01 08:29:27 -0600

    added routes and links to those routes in header

---
+++ react-todo/src/containers/TodosContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodosContainer extends Component {
+    render() {
+        return(
+            <h2>
+                this is the todos container
+            </h2>
+        )
+    }
+}
+
+export default TodosContainer;
